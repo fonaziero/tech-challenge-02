8cf6e7cacbcc65c350908f8bfb01c997836d0e88
Victor Carnevale <victorcarnevale@yahoo.com.br>
2025-01-21 00:49:45 -0300

    teste

--- frontend/dashboard/src/App.tsx
+++ frontend/dashboard/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from "./components/main";
+import './styles/tailwind.css'
 
 const NotFoundModule = await import('host/NotFound');
 const NotFound = NotFoundModule.default;
